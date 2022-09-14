54d7d3b7ba61583dccceff26a7c60163b4cad07a
VenkataGireesh <gireesh547@gmail.com>
2022-09-14 09:26:20 +0530

    repo created

---
+++ src/main/java/com/academy/admission/academyservice/AbacusAcademyAdmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.academy.admission.academyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacusAcademyAdmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacusAcademyAdmissionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/admission/academyservice/AbacusAcademyAdmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.admission.academyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacusAcademyAdmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
